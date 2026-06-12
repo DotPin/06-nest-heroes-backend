d547c5935d7e849e368b603c5ffb5d477536dfd4
dotpin <d.rojas.asenjo@gmail.com>
2026-06-12 17:11:00 -0400

    Backend to Heroes React Frontend

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000,process.env.HOST ?? '0.0.0.0');
+
+  console.log(`Server is running on port ${process.env.PORT ?? 3000} and host ${process.env.HOST ?? '0.0.0.0'}`);
+}
+void bootstrap();
